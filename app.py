c7c98253cde9eb4a2b7f0d3021c5b23c53da9884
Alvin N S <163659881+alvinns@users.noreply.github.com>
2024-09-11 11:34:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import google.generativeai as genai
+import streamlit as st
 
 genai.configure(api_key="AIzaSyD_hS7CZeAj01uBur9IemKkcf1sSR8rOLY")
+genai.GenerativeModel("gemini-1.5-flash")
+
+def getresponse(prompt):
+  response = generate_content(prompt)
+  return response
+
+prompt=st.chat_input("Enter your prompt")
+if prompt: 
+  response= getresponse(prompt)
+  st.write(response)
+
